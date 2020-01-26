9962515c727ddac1f2b0521afc385eefb836f511
AllanChain <AllanChain@users.noreply.github.com>
2020-01-26 07:49:02 +0000

    Automated deployment: Sun Jan 26 07:49:02 UTC 2020 52bbbe12d6b807b468de07d1064c8afc411d1722

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RelaTime/precache-manifest.5d1964aefd06bee7c8395c7103f01f0e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "todo"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("."));
+
+workbox.routing.registerRoute(/\//, new workbox.strategies.NetworkFirst({ "cacheName":"RelaTime","networkTimeoutSeconds":20, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
