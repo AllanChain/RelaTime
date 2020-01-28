e1e424dfa65a45cd0d95d6bb48a589f9a921b75a
Allan Chain <allanchain@pku.edu.cn>
2020-01-28 12:11:07 +0800

    use dialog

--- src/App.vue
+++ src/App.vue
@@ -26,9 +26,63 @@
 
       <v-spacer></v-spacer>
       <ICALProcessor />
+      <v-btn @click.stop="dialog = true" icon text small>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-content>
+      <v-dialog v-model="dialog" max-width="290">
+        <!-- <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">-->
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Modify Event</v-toolbar-title>
+            <v-spacer />
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn icon large target="_blank" v-on="on">
+                  <v-icon>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/pMvGQO"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Login" name="login" prepend-icon="place" type="text" />
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                prepend-icon="lock"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+        <!-- </v-col>
+        </v-row>-->
+      </v-dialog>
       <Calendar />
     </v-content>
   </v-app>
@@ -48,6 +102,7 @@ export default {
   data() {
     return {
       drawer: false,
+      dialog: false,
       bgImg: 'https://allanchain.github.io/webnav/back.jpg'
     }
   },
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// https://stackoverflow.com/a/57535771/8810271
 
 Vue.config.productionTip = false
 
